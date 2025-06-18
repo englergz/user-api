998c15349893ef01b8330d076fd99c9307abdd3b
Engler <englergonzalez@hotmail.com>
2025-06-18 14:48:24 -0500

    feat: exponer API en puerto 3000 con Docker Compose

--- internal/database/db.go
+++ internal/database/db.go
@@ -3,7 +3,8 @@ package database
 import (
 	"fmt"
 	"log"
-	
+	"time"
+
 
 	"github.com/englergz/user-api/internal/models"
 	"gorm.io/driver/postgres"
@@ -13,12 +14,21 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=secret dbname=users_db port=5432"
+	dsn := "host=postgres_db user=postgres password=secret dbname=users_db port=5432 sslmode=disable"
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	maxAttempts := 10
+
+	for i := 1; i <= maxAttempts; i++ {
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Intento %d: No se pudo conectar a la base de datos, reintentando en 3s...", i)
+		time.Sleep(3 * time.Second)
+	}
 
 	if err != nil {
-		log.Fatal("Error al conectar a la base de datos: ", err)
+		log.Fatalf("Error al conectar a la base de datos después de %d intentos: %v", maxAttempts, err)
 	}
 
 	DB.AutoMigrate(&models.User{})
